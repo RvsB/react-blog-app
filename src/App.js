eee8436d0f58286e8614d16f2a46cd26cd2f4510
Raj Vardhan Singh Birani <rbrajvardhan@gmail.com>
2022-03-27 01:55:48 +0530

    feat: added button to delete a blog and added functionality to it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50; //here it is a number, but it doesnt matter because react is gonna convert whatever datatype we use to a string before it outputs it to the browser
+  //const likes = 50; //here it is a number, but it doesnt matter because react is gonna convert whatever datatype we use to a string before it outputs it to the browser
 
   //react cannot output objects or booleans
 //const person = {name: "yoshi", age: "30"}; if we try to out put this it will result in an error there numbers, strings and arrays are fine and objects and booleans are not
