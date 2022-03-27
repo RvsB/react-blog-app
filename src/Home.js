36cceea53b0337288d13429e6995f1322e7baabe
Raj Vardhan Singh Birani <rbrajvardhan@gmail.com>
2022-03-28 02:07:43 +0530

    learned the use of useEffect hook

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -7,7 +7,9 @@ const Home = () => {
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
+    ]);
+
+    const [name, setName] = useState('mario');
 
 
     const handleDelete = (id) => {
@@ -15,10 +17,17 @@ const Home = () => {
         setBlogs(newBlogs);
     }
 
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(name);
+    }, [name]); //normally inside this function, inside this useEffect we could do something like fetch data or communicate with some aunthetication service and those things are known as side effects in react
+
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete} />
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs!" />
+            <button onClick={() => setName('luigi')}>change name</button>
+            <p>{ name }</p>
+            {/*<BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs!" />*/}
         </div>
     );
 }
