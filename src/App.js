fcca3226dcb5b40e7df1c476b4a4713c5b573bc9
Raj Vardhan Singh Birani <rbrajvardhan@gmail.com>
2022-03-29 19:46:10 +0530

    learned about custom hooks, react routers and useEffect cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
   //const likes = 50; //here it is a number, but it doesnt matter because react is gonna convert whatever datatype we use to a string before it outputs it to the browser
@@ -7,14 +9,23 @@ function App() {
   //react cannot output objects or booleans
 //const person = {name: "yoshi", age: "30"}; if we try to out put this it will result in an error there numbers, strings and arrays are fine and objects and booleans are not
 
-  return (
-    <div className="App">
-      <Navbar /> {/*we can also write it as <Navbar></Navbar>*/}
-      <div className="content">
-        <Home />
-      </div>
-    </div>
-  );
+	return (
+    	<Router> {/* we surround our entire application with the router component and what that means is we can use the router in the entire application, all components that are nested inside this app component have acces to the router */}
+			<div className="App">
+				<Navbar /> {/*we can also write it as <Navbar></Navbar>*/}
+				<div className="content">
+					<Switch> {/*switch component makes sure that only one route shows at any one time and all of our routes go inside the switch component */}
+						<Route exact path="/"> {/* we create a route for each page that we have using this route component. The 'path' property is basically the route, so for home page it will be "/", if it were a contact page it could have been "/contact", basically this is the path after the root of our website */}
+							<Home />
+						</Route>
+						<Route path="/create"> 
+							<Create />
+						</Route>
+					</Switch>
+				</div>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    //let name = 'mario';
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
+
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs'); //here the data: blogs means that grab the data and call it blogs in the current scope
+
+    // const [blogs, setBlogs] = useState(null);
+    // const [isPending, setIsPending] = useState(true);
+    // const [error, setError] = useState(null);
 
 
     // const handleDelete = (id) => {
@@ -13,32 +16,32 @@ const Home = () => {
     //     setBlogs(newBlogs);
     // }
 
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-            .then(res => {
-                if(!res.ok) {
-                    throw Error('could not fetch the data for that resource')
-                }
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-                setIsPending(false);
-                setError(null);
-            })
-            .catch((err) => {
-                setIsPending(false);
-                setError(err.message);
-            })
-        }, 2000);
-    }, []); //normally inside this function, inside this useEffect we could do something like fetch data or communicate with some aunthetication service and those things are known as side effects in react
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         fetch('http://localhost:8000/blogs')
+    //         .then(res => {
+    //             if(!res.ok) {
+    //                 throw Error('could not fetch the data for that resource')
+    //             }
+    //             return res.json();
+    //         })
+    //         .then(data => {
+    //             setBlogs(data);
+    //             setIsPending(false);
+    //             setError(null);
+    //         })
+    //         .catch((err) => {
+    //             setIsPending(false);
+    //             setError(err.message);
+    //         })
+    //     }, 2000);
+    // }, []); //normally inside this function, inside this useEffect we could do something like fetch data or communicate with some aunthetication service and those things are known as side effects in react
 
     return ( 
         <div className="home">
             { error && <div>{ error }</div> }
             { isPending && <div>Loading...</div> }
-            {blogs && <BlogList blogs={blogs} title="All blogs!" />}
+            { blogs && <BlogList blogs={blogs} title="All blogs!" /> }
             {/*<BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs!" />*/}
         </div>
     );
